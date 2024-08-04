2988d4a5662546ce131918d0c3137e1cb866977a
Harshit Agarwal <harshitagarwalwork21@gmail.com>
2024-08-04 17:13:22 +0530

    need

--- home/views.py
+++ home/views.py
@@ -16,53 +16,5 @@ def contact_view(request):
 
 
 
-
-
-
-
-
-
 def index(request):
     return render(request, 'index.html')
-
-
-# def Contact(request):
-#     if request.method == "POST":
-#       data = request.POST
-
-#       name = data.get('name')
-#       email= data.get('email')
-#       message = data.get('message')
-
-     
-#       Contact.objects.create(
-#         message=message,
-#         email=email,
-#         name= name,
-#         )
-      
-#       return redirect('/contact')
-#     queryset = Contact.objects.all()
-#     context = {'contact': queryset}
-
-    
-#     messages.success(request, "We will contact you shortly")
-#     return render(request, 'index.html',context)
-
-
-
-# def contact_view(request):
-#     if request.method == 'POST':
-#         form = ContactForm(request.POST)
-#         if form.is_valid():
-#             form.save()
-#             return redirect('success')
-#     else:
-#         form = ContactForm()
-#     return render(request, 'contact.html', {'form': form})
-
-# def success_view(request):
-#     messages.success(request, "We will contact you shortly")
-#     return render(request, 'index.html')
-
-# Create your views here.
--- portfolio/settings.py
+++ portfolio/settings.py
@@ -25,7 +25,7 @@ TEMPLATE_DIR = os.path.join(BASE_DIR , 'template')
 SECRET_KEY = "django-insecure-i2=8x3_51vz268w5lsp(3i8jpal4m6q5&&9krx00lkd0wev!^#"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -117,12 +117,14 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
-import os
 
 STATIC_URL = "static/"
 STATIC_DIR= os.path.join(BASE_DIR,'static')
 
 STATICFILES_DIRS =[STATIC_DIR]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
