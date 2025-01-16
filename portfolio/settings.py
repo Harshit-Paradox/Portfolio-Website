70fdca1fa98371a60f277475fa34983ffe666dcb
Harshit Agarwal <harshitagarwalwork21@gmail.com>
2025-01-16 20:29:46 +0530

    for vercel

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = "django-insecure-i2=8x3_51vz268w5lsp(3i8jpal4m6q5&&9krx00lkd0wev!^#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.vercel.app', '*']
 
 
 # Application definition
